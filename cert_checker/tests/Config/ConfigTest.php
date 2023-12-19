02b3a69a44fda170c30809bd095af97939f9db8b
SidAlex <palach.orl@mail.ru>
2023-12-19 09:52:40 +0300

    add covers annotation

--- cert_checker/tests/Config/ConfigTest.php
+++ cert_checker/tests/Config/ConfigTest.php
@@ -4,6 +4,9 @@ namespace Sidalex\CertChecker\Classes\Config;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @uses \Sidalex\CertChecker\Classes\Config\Config
+ */
 class ConfigTest extends TestCase
 {
 
@@ -14,44 +17,63 @@ class ConfigTest extends TestCase
 
     /**
      * @dataProvider envDataProvider
+     * @covers       \Sidalex\CertChecker\Classes\Config\Config::__construct
+     * @covers       \Sidalex\CertChecker\Classes\RequestSandler\RequestParamsBuilderFromEnv
      */
-    public function test_config_construct($env){
-        $env =[
-            "URLS"=>"http://localhost,http://localhost1,http://localhost2,http://localhost3,http://localhost4"
-        ];
-        $config = new Config($env);
-        $this->assertIsArray($config->getUrlsToCheck(),"url in config is not array");
-
-    }
-    public function test_getUrlsToCheck(){
-        $env =[
-            "URLS"=>"http://localhost,http://localhost1,http://localhost2,http://localhost3,http://localhost4"
-        ];
-        $config = new Config($env);
-        $this->assertIsArray($config->getUrlsToCheck(),"url in config is not array");
-
-        $env =[
-            "URLS"=>"http://localhost"
-        ];
-        $config = new Config($env);
-        $this->assertEquals($config->getUrlsToCheck()[0],'http://localhost',"config.url in config is not valid");
-
-        $env =[
-        ];
-        $config = new Config($env);
-        $this->assertEmpty($config->getUrlsToCheck(),"config.url in config is not empty");
-
-
-    }
-
-    public static function envDataProvider()
+    public function test_construct($env)
     {
-        return  [
+        $config = new Config($env);
+        $this->assertIsArray($config->getUrlsToCheck(), "url in config is not array");
+        $this->assertIsInt($config->getCountDaysToWorn(), 'count days to worn is not integer');
+
+    }
+
+    /**
+     * @covers \Sidalex\CertChecker\Classes\Config\Config
+     * @covers Sidalex\CertChecker\Classes\RequestSandler\RequestParamsBuilderFromEnv
+     * @covers \Sidalex\CertChecker\Classes\Config\Config::getUrlsToCheck
+     */
+    public function test_getUrlsToCheck()
+    {
+        $env = [
+            "URLS" => "http://localhost,http://localhost1,http://localhost2,http://localhost3,http://localhost4"
+        ];
+        $config = new Config($env);
+        $this->assertIsArray($config->getUrlsToCheck(), "url in config is not array");
+
+        $env = [
+            "URLS" => "http://localhost"
+        ];
+        $config = new Config($env);
+        $this->assertEquals($config->getUrlsToCheck()[0], 'http://localhost', "config.url in config is not valid");
+
+        $env = [
+        ];
+        $config = new Config($env);
+        $this->assertEmpty($config->getUrlsToCheck(), "config.url in config is not empty");
+
+
+    }
+
+    public static function envDataProvider(): array
+    {
+        return [
             [
-                "URLS"=>"http://test1.com,https://test2.com,http://test3.com"
+                [
+                    "URLS" => "http://test1.com,https://test2.com,http://test3.com",
+                    "COUNT_DAYS_TO_WORN" => 1,
+                ]
             ],
             [
-                "URLS"=>"http://localhost,http://localhost1,http://localhost2,http://localhost3,http://localhost4"
+                [
+                    "URLS" => "http://localhost,http://localhost1,http://localhost2,http://localhost3,http://localhost4",
+                    "COUNT_DAYS_TO_WORN" => 5,
+                ]
+            ],
+            [
+                [
+                    "test" => 'test'
+                ]
             ],
         ];
     }
